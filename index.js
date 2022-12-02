99b2d63f2c5d0d27b58435ea78c82053b0bee4e6
Dominic Lizarraga <70678718+DominicLizarraga@users.noreply.github.com>
2022-12-01 23:50:24 -0800

    add a function to the form

--- index.js
+++ index.js
@@ -1,16 +1,30 @@
 const list = document.querySelector(".list-inline")
 console.log(list)
 
-fetch("http://www.omdbapi.com/?s=harry potter&apikey=adf1f2d7")
-  .then(response => response.json())
-  .then((data) => {
-    data.Search.forEach((result) => {
-      console.log(result.Title)
-        const movie = `<li class="list-inline-item">
-    <img src="${result.Poster}" alt="">
-    <p>${result.Title}</p>
-    </li>`
-    list.insertAdjacentHTML("beforeend", movie)
+
+fetchMovies = (query) => {
+  fetch(`http://www.omdbapi.com/?s=${query}&apikey=adf1f2d7`)
+    .then(response => response.json())
+    .then((data) => {
+      data.Search.forEach((result) => {
+        console.log(result.Title)
+          const movie = `<li class="list-inline-item">
+          <img src="${result.Poster}" alt="">
+          <p>${result.Title}</p>
+          </li>`
+          list.insertAdjacentHTML("beforeend", movie)
+    })
   })
+};
+
+const form = document.getElementById("search-form")
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const input = event.currentTarget.querySelector(".form-control")
+  list.innerHTML = ""
+  fetchMovies(input.value)
+
 })
+
 
