df00941ac8682517a4cbf7fc98b6ed4030a2d188
Dominic Lizarraga <70678718+DominicLizarraga@users.noreply.github.com>
2022-12-02 23:18:55 -0800

    post request done

--- index.js
+++ index.js
@@ -1,30 +1,51 @@
-const list = document.querySelector(".list-inline")
-console.log(list)
+// GET request
+
+// const list = document.querySelector(".list-inline")
+// console.log(list)
 
 
-fetchMovies = (query) => {
-  fetch(`http://www.omdbapi.com/?s=${query}&apikey=adf1f2d7`)
+// fetchMovies = (query) => {
+//   fetch(`http://www.omdbapi.com/?s=${query}&apikey=adf1f2d7`)
+//     .then(response => response.json())
+//     .then((data) => {
+//       data.Search.forEach((result) => {
+//         console.log(result.Title)
+//           const movie = `<li class="list-inline-item">
+//           <img src="${result.Poster}" alt="">
+//           <p>${result.Title}</p>
+//           </li>`
+//           list.insertAdjacentHTML("beforeend", movie)
+//     })
+//   })
+// };
+
+// const form = document.getElementById("search-form")
+
+// form.addEventListener("submit", (event) => {
+//   event.preventDefault();
+//   const input = event.currentTarget.querySelector(".form-control")
+//   list.innerHTML = ""
+//   fetchMovies(input.value)
+
+// })
+
+
+// POST request
+
+const signUp = (event) => {
+  event.preventDefault()
+  const emailValue = document.getElementById("email").value
+  const passwordValue = document.getElementById("password").value
+  fetch("https://reqres.in/api/register", {
+    method: "POST",
+    headers: {"Content-Type": "application/json"},
+    body: JSON.stringify({"email": emailValue, "password": passwordValue})
+  })
     .then(response => response.json())
     .then((data) => {
-      data.Search.forEach((result) => {
-        console.log(result.Title)
-          const movie = `<li class="list-inline-item">
-          <img src="${result.Poster}" alt="">
-          <p>${result.Title}</p>
-          </li>`
-          list.insertAdjacentHTML("beforeend", movie)
+      console.log(data)
     })
-  })
-};
+}
 
-const form = document.getElementById("search-form")
-
-form.addEventListener("submit", (event) => {
-  event.preventDefault();
-  const input = event.currentTarget.querySelector(".form-control")
-  list.innerHTML = ""
-  fetchMovies(input.value)
-
-})
-
-
+const form = document.querySelector("#form")
+form.addEventListener("submit", signUp)
